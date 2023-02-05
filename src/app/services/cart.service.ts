39430e1506e2c31ab20512190839638417d1e9a7
Emmanuel Zelarayan <emmavz@live.com>
2023-02-05 20:45:14 -0300

    implementing cart logic

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -26,4 +26,49 @@ export class CartService {
     this._snackBar.open("1 item added to cart.", "Ok", { duration: 3000 });
     console.log(this.cart.value);
   }
+
+  getTotal(items: Array<CartItem>): number {
+    return items.map((item) => item.price * item.quantity).reduce((prev, current) => prev + current, 0);
+  }
+
+  clearCart(): void {
+    this.cart.next({ items: [] });
+    this._snackBar.open("Cart is cleared", "Ok", { duration: 3000 });
+  }
+
+  removeFromCart(item: CartItem, update = true): Array<CartItem> {
+    const filteredItems = this.cart.value.items.filter((_item) => _item.id !== item.id);
+
+    if (update) {
+      this.cart.next({ items: filteredItems });
+      this._snackBar.open("1 item removed from cart.", "OK", {
+        duration: 3000,
+      });
+    }
+
+    return filteredItems;
+  }
+
+  removeQuantity(item: CartItem): void {
+    let itemForRemoval: CartItem | undefined;
+
+    let filtederItems = this.cart.value.items.map((_item) => {
+      if (_item.id === item.id) {
+        _item.quantity -= 1;
+
+        if (_item.quantity === 0) {
+          itemForRemoval = _item;
+        }
+      }
+
+      return _item;
+    });
+
+    if (itemForRemoval) {
+      filtederItems = this.removeFromCart(itemForRemoval, false);
+    }
+
+    this.cart.next({ items: filtederItems });
+    this._snackBar.open("1 item removed from cart.", "Ok", { duration: 3000 });
+  }
 }
